ab03a4c59e20d7b249b0ead6546183308c97e6bc
Miguel Angel Sanchez Arenas <miguel.sanchez@cfe.mx>
2019-01-21 14:35:13 -0600

    Se agrega estilo de bootstrap

--- src/LAPEM.Informatica.GestionSw.Presentacion/App_Start/BundleConfig.cs
+++ src/LAPEM.Informatica.GestionSw.Presentacion/App_Start/BundleConfig.cs
@@ -8,7 +8,16 @@ namespace LAPEM.Informatica.GestionSw.Presentacion
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/scripts")              //.Include("~/app/scripts/jquery.min.js")
+              
+              .Include("~/app/Scripts/jquery-3.3.1.js")
+              .Include("~/app/Scripts/bootstrap.js")
 
+            );
+
+            bundles.Add(new StyleBundle("~/bundles/styles")
+                .Include("~/app/Css/bootstrap.css")
+            );
         }
     }
 }
